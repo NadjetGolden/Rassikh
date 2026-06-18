80690141f15602d2e2bec85b69f14ff7a306aae3
Nadjet <41029974+NadjetGolden@users.noreply.github.com>
2026-06-18 19:57:18 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,26 +1,23 @@
-const CACHE_NAME = 'rasikh-v1';
-const ASSETS = ['./index.html', './icon-192x192.png', './icon-512x512.png'];
+var CACHE_NAME = 'rasikh-v1';
+var ASSETS = ['./index.html', './icon-192x192.png', './icon-512x512.png', './manifest.json'];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(ASSETS);
+    })
   );
   self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
+self.addEventListener('activate', function(event) {
   self.clients.claim();
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then(cached => {
-      return cached || fetch(event.request).catch(() => caches.match('./index.html'));
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
     })
   );
 });
